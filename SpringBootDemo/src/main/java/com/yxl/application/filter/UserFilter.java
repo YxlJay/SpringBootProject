5035fc3bb0d20df3689d3f3490fa47afecb22730
yxl <y1152457189@163.com>
2023-05-26 14:02:11 +0800

    6

--- SpringBootDemo/src/main/java/com/yxl/application/filter/UserFilter.java
+++ SpringBootDemo/src/main/java/com/yxl/application/filter/UserFilter.java
@@ -15,6 +15,9 @@ public class UserFilter  implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("user");
-        filterChain.doFilter(servletRequest,servletResponse);
+       filterChain.doFilter(servletRequest,servletResponse);
+
     }
+
+
 }
